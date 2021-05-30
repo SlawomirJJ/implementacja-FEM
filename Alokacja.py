306a4376f1fce49df24bcc9ef7acaf5dafb39543
SlawomirJJ <sawomirjedziniak@gmail.com>
2021-05-30 23:01:56 +0200

    implementacja

---
+++ Alokacja.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+def Alokacja(n):
+    
+    A = np.zeros([n,n])
+    b = np.zeros([n,1])
+
+    return A,b
---
+++ RysujRozwiazanie.py
@@ -0,0 +1,11 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from RysujGeometrie import *
+
+def RysujRozwiazanie(NODES, ELEMS, WB, u):
+   
+    RysujGeometrie(NODES,ELEMS,WB)
+    
+    x = NODES[:,1]
+    
+    plt.plot(x, u, 'm*')
